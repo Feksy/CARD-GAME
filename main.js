3c9dad8c79fcdeaa4570d91a012654601aa79c58
Feksy <lenab1563@gmail.com>
2022-07-15 06:23:26 -0400

    changed js

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
 const LEVELS = {
     easy: {
-        cards: 6
+        cards: 6,
     },
     medium: {
-        cards: 12
+        cards: 12,
     },
     hard: {
-        cards: 18
-    }
+        cards: 18,
+    },
 }
 
-const levelButtons = document.querySelectorAll('.level-button');
-const startButton = document.querySelectorAll('.start-button');
-let userLevel = '';
-const firstLevelButton = document.querySelectorAll('.first-level-button');
-const secondLevelButton = document.querySelectorAll('.second-level-button');
-const thirdLevelButton = document.querySelectorAll('.third-level-button');
+const levelButtons = document.querySelectorAll('.level-button')
+const startButton = document.querySelector('.start-button')
+let userLevel = ''
 
-
-levelButtons.forEach(button => button.addEventListener('click', (button) => {
-    button.preventDefault();
-    const target = button.target;
-    userLevel = target.value;
-    console.log(target);
-    levelButtons.forEach(b => {
-        if (b.value !== target.value) {
-            b.classList.remove("level-selected");
-        }
+levelButtons.forEach((button) =>
+    button.addEventListener('click', (button) => {
+        button.preventDefault()
+        const target = button.target
+        userLevel = target.value
+        levelButtons.forEach((b) => {
+            if (b.value !== target.value) {
+                b.classList.remove('level-selected')
+            }
+        })
+        target.classList.add('level-selected')
     })
-    target.classList.add("level-selected");
-
-}))
-
+)
